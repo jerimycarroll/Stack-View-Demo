1a489f70dbc6eac2ff639275ad047fa542840d0e
Jerimy Carroll <jerimycarroll@mac.com>
2023-04-18 21:47:22 -0700

    Initial Commit

---
+++ StackViewDemo/StackViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewDemoApp.swift
+//  StackViewDemo
+//
+//  Created by Jerimy Carroll on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct StackViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
