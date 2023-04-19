a07d3ce686add41fc4a65ad8f1993cd4aa91a878
jerimycarroll <jerimycarroll@mac.com>
2023-04-18 22:21:16 -0700

    Learning slowly but surely but will get it done

--- StackViewDemo/ContentView.swift
+++ StackViewDemo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world of source code comitting again and again and again and again one more time")
+            Text("Hello, world of source code comitting again and again and again and again one more time test")
         }
         .padding()
     }
